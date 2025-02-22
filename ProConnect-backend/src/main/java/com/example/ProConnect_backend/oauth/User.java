a39b1912329d100d587e1b61ad261e5f848ba5b5
gaurang5425 <163129474+gaurang5425@users.noreply.github.com>
2025-02-22 16:56:57 +0530

    google authentication

---
+++ ProConnect-backend/src/main/java/com/example/ProConnect_backend/oauth/User.java
@@ -0,0 +1,66 @@
+package com.example.ProConnect_backend.oauth;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table()
+public class User{
+    User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+    private String picture;
+    private String googleId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getGoogleId() {
+		return googleId;
+	}
+	public void setGoogleId(String googleId) {
+		this.googleId = googleId;
+	}
+	User( String email, String name, String picture, String googleId) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.picture = picture;
+		this.googleId = googleId;
+	}
+    
+    
+    // Add any additional fields you want to store
+}
